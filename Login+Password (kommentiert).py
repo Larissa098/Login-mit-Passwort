e16b1efffcf135dca17292c6477039122d9ffbc2
Larissa098 <97455290+Larissa098@users.noreply.github.com>
2022-01-10 15:34:01 +0100

    Code Login
    Code wurde hochgeladen

---
+++ Login+Password (kommentiert).py
@@ -0,0 +1,21 @@
+x=input("geben sie bitte ihr Benutzername ein: ")
+z=input("Bitte geben sie ihr Passwort ein: ")
+if x == "Anna_Stöckli"and z =="coolheit": 
+    
+    print("Herzlich Willkommen Anna")
+elif x == "Larissa_Carver"and z =="hallo123":
+    print("Herzlich Willkommen Larissa")
+elif x == "Sophie_Germann" and z == "grüezi456":
+    print("Herzlich Willkommen Sophie")
+elif x == "Michelle_Koch" and z == "holyguacamole" :
+    print("Herzlich Willkommen Michelle")
+elif x == "Anna_Streit" and z == "gruziii":
+    print("Herzlich Willkommen Anna")
+elif x == "Katja_Schär" and z == "dinosaurier724":
+    print("Herzlich Willkommen Katja")
+elif x =="Oliwia_Lyszczarz" and z == "macka packa":
+    print("Herzlic Willkommen Oliwia")
+elif x ==("Olivia_Schär") and z == "burger93":
+    print("Herzlic Willkommen Olivia")
+else: 
+    print("Benutzername und/oder Passwort nicht korrekt")
