d5c05cb3b4857d246f7f11f2593a4b6e3638a6f8
Larissa098 <97455290+Larissa098@users.noreply.github.com>
2022-01-10 15:38:34 +0100

    Update Benutzernamen

--- Login+Password (kommentiert).py
+++ Login+Password (kommentiert).py
@@ -1,21 +1,9 @@
 x=input("geben sie bitte ihr Benutzername ein: ")
 z=input("Bitte geben sie ihr Passwort ein: ")
-if x == "Anna_Stöckli"and z =="coolheit": 
+if x == "Anna_Müller"and z =="coolheit": 
     
     print("Herzlich Willkommen Anna")
-elif x == "Larissa_Carver"and z =="hallo123":
-    print("Herzlich Willkommen Larissa")
-elif x == "Sophie_Germann" and z == "grüezi456":
-    print("Herzlich Willkommen Sophie")
-elif x == "Michelle_Koch" and z == "holyguacamole" :
-    print("Herzlich Willkommen Michelle")
-elif x == "Anna_Streit" and z == "gruziii":
-    print("Herzlich Willkommen Anna")
-elif x == "Katja_Schär" and z == "dinosaurier724":
-    print("Herzlich Willkommen Katja")
-elif x =="Oliwia_Lyszczarz" and z == "macka packa":
-    print("Herzlic Willkommen Oliwia")
-elif x ==("Olivia_Schär") and z == "burger93":
-    print("Herzlic Willkommen Olivia")
+elif x == "Max_Meyer"and z =="hallo123":
+
 else: 
     print("Benutzername und/oder Passwort nicht korrekt")
