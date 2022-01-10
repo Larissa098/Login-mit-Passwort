e81fae7ef5c6056436304be27747dd96b49e894c
Larissa098 <97455290+Larissa098@users.noreply.github.com>
2022-01-10 15:39:16 +0100

    Update Benutzernamen

--- Login+Password (kommentiert).py
+++ Login+Password (kommentiert).py
@@ -1,9 +1,16 @@
+# Das Programm fragt sie nach ihrem Benutzernamen und Passwort
 x=input("geben sie bitte ihr Benutzername ein: ")
 z=input("Bitte geben sie ihr Passwort ein: ")
-if x == "Anna_Müller"and z =="coolheit": 
-    
-    print("Herzlich Willkommen Anna")
-elif x == "Max_Meyer"and z =="hallo123":
 
+
+########################################################################################################
+# Falls der Benutzername Anna_Müller und das Passwort coolheit ist, wird "Herzlich Willkommen Anna" ausgegeben. 
+if x == "Anna_Stöckli"and z =="coolheit": 
+    print("Herzlich Willkommen Anna")
+#Falls der Benutzername Max_Meyer und das Passwort hallo123 ist, wird "Herzlich Willkommen Max" ausgegeben. 
+elif x == "Max_Meyer"and z =="hallo123":
+    print("Herzlich Willkommen Max")
+# Falls keiner dieser Benutzernamen/Passwörter eingegeben wurden,wird ein Fehler ausgegeben. 
 else: 
     print("Benutzername und/oder Passwort nicht korrekt")
+########################################################################################################
